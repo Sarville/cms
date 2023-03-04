c09a084dc86076a78a6c26a183dc753170597c2c
Sarville <sarville@gmail.com>
2023-03-04 20:46:20 +0300

    module2 topic2

---
+++ script/script.js
@@ -0,0 +1,5 @@
+'use strict';
+const head = document.querySelector('head');
+const form =document.querySelector('.modal__form');
+const checkbox =document.querySelector('.modal__checkbox');
+const discount =document.querySelector('.modal__input_discount');
